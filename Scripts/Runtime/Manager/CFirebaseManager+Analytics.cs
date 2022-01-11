0e8381dea6db0f0f92b389c39fcc846214eb50ab
sd.lee <sd.lee@ninetap.com>
2022-01-11 13:54:15 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CFirebaseManager+Analytics.cs
+++ Scripts/Runtime/Manager/CFirebaseManager+Analytics.cs
@@ -43,7 +43,7 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 
 #if AUTO_LOG_PARAMS_ENABLE
 #if ANALYTICS_TEST_ENABLE || (DEBUG || DEVELOPMENT_BUILD)
-			oDataDict.ExAddVal(KCDefine.L_LOG_KEY_USER_TYPE, KCDefine.B_UNKNOWN_STR);
+			oDataDict.ExAddVal(KCDefine.L_LOG_KEY_USER_TYPE, KCDefine.B_TEXT_UNKNOWN);
 #else
 			oDataDict.ExAddVal(KCDefine.L_LOG_KEY_USER_TYPE, CCommonUserInfoStorage.Inst.UserInfo.UserType.ToString());
 #endif			// #if ANALYTICS_TEST_ENABLE || (DEBUG || DEVELOPMENT_BUILD)
