96aa0a5a40809cc31bbd6c4c1e3b9e800cc28523
sd.lee <sd.lee@ninetap.com>
2022-04-29 21:46:38 +0900

    [이상동] RemoteConfig 관련 구문 제거

--- Scripts/Runtime/Manager/CFirebaseManager.cs
+++ Scripts/Runtime/Manager/CFirebaseManager.cs
@@ -15,10 +15,6 @@ using Firebase.Auth;
 using Firebase.Analytics;
 #endif			// #if FIREBASE_ANALYTICS_ENABLE
 
-#if FIREBASE_REMOTE_CONFIG_ENABLE
-using Firebase.RemoteConfig;
-#endif			// #if FIREBASE_REMOTE_CONFIG_ENABLE
-
 #if FIREBASE_CLOUD_MSG_ENABLE
 using Firebase.Messaging;
 #endif			// #if FIREBASE_CLOUD_MSG_ENABLE
@@ -45,22 +41,16 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 		LOAD_DB,
 #endif			// #if FIREBASE_DB_ENABLE
 
-#if FIREBASE_REMOTE_CONFIG_ENABLE
-		LOAD_CONFIG,
-#endif			// #if FIREBASE_REMOTE_CONFIG_ENABLE
-
 		[HideInInspector] MAX_VAL
 	}
 
 	/** 매개 변수 */
 	public struct STParams {
-		public Dictionary<string, object> m_oConfigDict;
 		public Dictionary<ECallback, System.Action<CFirebaseManager, bool>> m_oCallbackDict;
 	}
 
 	#region 변수
 	private STParams m_stParams;
-	private bool m_bIsSetupDefConfigs = false;
 	private FirebaseApp m_oFirebaseApp = null;
 
 	private Dictionary<EFirebaseCallback, System.Action<CFirebaseManager, bool>> m_oCallbackDict01 = new Dictionary<EFirebaseCallback, System.Action<CFirebaseManager, bool>>();
@@ -81,16 +71,6 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 		}
 	}
 
-	public bool IsSetupDefConfigs {
-		get {
-#if (UNITY_IOS || UNITY_ANDROID) && FIREBASE_REMOTE_CONFIG_ENABLE
-			return this.IsInit && m_bIsSetupDefConfigs;
-#else
-			return false;
-#endif			// #if (UNITY_IOS || UNITY_ANDROID) && FIREBASE_REMOTE_CONFIG_ENABLE
-		}
-	}
-
 	public string UserID {
 		get {
 #if (UNITY_IOS || UNITY_ANDROID) && FIREBASE_AUTH_ENABLE
@@ -105,8 +85,7 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 	#region 함수
 	/** 초기화 */
 	public virtual void Init(STParams a_stParams) {
-		CFunc.ShowLog($"CFirebaseManager.Init: {a_stParams.m_oConfigDict}", KCDefine.B_LOG_COLOR_PLUGIN);
-		CAccess.Assert(a_stParams.m_oConfigDict != null);
+		CFunc.ShowLog($"CFirebaseManager.Init", KCDefine.B_LOG_COLOR_PLUGIN);
 
 #if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
 		// 초기화 되었을 경우
@@ -146,13 +125,6 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 #endif			// #if ANALYTICS_TEST_ENABLE || STORE_DIST_BUILD
 #endif			// #if FIREBASE_ANALYTICS_ENABLE
 
-#if FIREBASE_REMOTE_CONFIG_ENABLE
-				// 속성이 유효 할 경우
-				if(m_stParams.m_oConfigDict != null) {
-					CTaskManager.Inst.WaitAsyncTask(FirebaseRemoteConfig.DefaultInstance.SetDefaultsAsync(m_stParams.m_oConfigDict), this.OnSetupDefConfigs);
-				}
-#endif			// #if FIREBASE_REMOTE_CONFIG_ENABLE
-
 #if FIREBASE_CLOUD_MSG_ENABLE
 				FirebaseMessaging.TokenReceived += this.OnReceiveMsgToken;
 				FirebaseMessaging.MessageReceived += this.OnReceiveNotiMsg;
@@ -162,13 +134,6 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 			m_stParams.m_oCallbackDict?.GetValueOrDefault(ECallback.INIT)?.Invoke(this, this.IsInit);
 		});
 	}
-
-#if FIREBASE_REMOTE_CONFIG_ENABLE
-	/** 기본 속성을 설정했을 경우 */
-	private void OnSetupDefConfigs(Task a_oTask) {
-		m_bIsSetupDefConfigs = a_oTask.ExIsCompleteSuccess();
-	}
-#endif			// #if FIREBASE_REMOTE_CONFIG_ENABLE
 #endif			// #if UNITY_IOS || UNITY_ANDROID
 	#endregion			// 조건부 함수
 }
