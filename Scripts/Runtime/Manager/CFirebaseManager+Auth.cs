fe6fa2dc2d364ca423355874070c1a6649a6766e
sd.lee <sd.lee@ninetap.com>
2022-05-23 07:16:32 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CFirebaseManager+Auth.cs
+++ Scripts/Runtime/Manager/CFirebaseManager+Auth.cs
@@ -13,8 +13,8 @@ using Firebase.Auth;
 public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 	#region 함수
 	/** 익명 로그인을 처리한다 */
-	public void Login(System.Action<CFirebaseManager, bool> a_oCallback) {
-		CFunc.ShowLog("CFirebaseManager.Login", KCDefine.B_LOG_COLOR_PLUGIN);
+	public void Login(string a_oDeviceID, System.Action<CFirebaseManager, bool> a_oCallback) {
+		CFunc.ShowLog($"CFirebaseManager.Login: {a_oDeviceID}", KCDefine.B_LOG_COLOR_PLUGIN);
 
 #if (UNITY_IOS || UNITY_ANDROID) && FIREBASE_AUTH_ENABLE
 		// 로그인 되었을 경우
@@ -53,25 +53,6 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 #else
 		CFunc.Invoke(ref a_oCallback, this, false);
 #endif			// #if (UNITY_IOS || UNITY_ANDROID) && (FIREBASE_AUTH_ENABLE && FACEBOOK_MODULE_ENABLE)
-	}
-
-	/** 게임 센터 로그인을 처리한다 */
-	public void LoginWithGameCenter(string a_oAccessToken, System.Action<CFirebaseManager, bool> a_oCallback) {
-		CFunc.ShowLog($"CFirebaseManager.LoginWithGameCenter: {a_oAccessToken}", KCDefine.B_LOG_COLOR_PLUGIN);
-		CAccess.Assert(a_oAccessToken.ExIsValid());
-
-#if (UNITY_IOS || UNITY_ANDROID) && (FIREBASE_AUTH_ENABLE && GAME_CENTER_MODULE_ENABLE)
-		var oAuth = FirebaseAuth.DefaultInstance;
-		
-#if UNITY_IOS
-		m_oCallbackDict01.ExReplaceVal(EFirebaseCallback.LOGIN, a_oCallback);
-		CTaskManager.Inst.WaitAsyncTask(GameCenterAuthProvider.GetCredentialAsync(), this.OnReceiveGameCenterCredential);	
-#else
-		this.LoginWithCredential(PlayGamesAuthProvider.GetCredential(a_oAccessToken), a_oCallback);
-#endif			// #if UNITY_IOS
-#else
-		CFunc.Invoke(ref a_oCallback, this, false);
-#endif			// #if (UNITY_IOS || UNITY_ANDROID) && (FIREBASE_AUTH_ENABLE && GAME_CENTER_MODULE_ENABLE)
 	}
 	
 	/** 로그아웃을 처리한다 */
@@ -113,22 +94,6 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 			CTaskManager.Inst.WaitAsyncTask(FirebaseAuth.DefaultInstance.SignInWithCredentialAsync(a_oCredential), this.OnLogin);
 		}
 	}
-
-#if UNITY_IOS && GAME_CENTER_MODULE_ENABLE
-	/** 게임 센터 인증을 수신했을 경우 */
-	private void OnReceiveGameCenterCredential(Task<Credential> a_oTask) {
-		CFunc.ShowLog($"CFirebaseManager.OnReceiveGameCenterCredential: {a_oTask.ExIsCompleteSuccess()}");
-		
-		CScheduleManager.Inst.AddCallback(KCDefine.U_KEY_FIREBASE_M_RECEIVE_GAME_CENTER_CREDENTIAL_CALLBACK, () => {
-			// 수신했을 경우
-			if(a_oTask.ExIsCompleteSuccess()) {
-				this.LoginWithCredential(a_oTask.Result, m_oCallbackDict01.GetValueOrDefault(EFirebaseCallback.LOGIN));
-			} else {
-				m_oCallbackDict01.GetValueOrDefault(EFirebaseCallback.LOGIN)?.Invoke(this, false);
-			}
-		});
-	}
-#endif			// #if UNITY_IOS && GAME_CENTER_MODULE_ENABLE
 #endif			// #if (UNITY_IOS || UNITY_ANDROID) && FIREBASE_AUTH_ENABLE
 	#endregion			// 조건부 함수
 }
