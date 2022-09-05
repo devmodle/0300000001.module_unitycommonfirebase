ba8924c236858ecd2cded9ee7043ef22e992ff9d
sd.lee <sd.lee@ninetap.com>
2022-09-05 23:50:34 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CFirebaseManager.cs
+++ Scripts/Runtime/Manager/CFirebaseManager.cs
@@ -165,9 +165,9 @@ public partial class CFirebaseManager : CSingleton<CFirebaseManager> {
 
 	#region 클래스 함수
 	/** 매개 변수를 생성한다 */
-	public static STParams MakeParams(Dictionary<ECallback, System.Action<CFirebaseManager, bool>> a_oCallbackDict) {
+	public static STParams MakeParams(Dictionary<ECallback, System.Action<CFirebaseManager, bool>> a_oCallbackDict = null) {
 		return new STParams() {
-			m_oCallbackDict = a_oCallbackDict
+			m_oCallbackDict = a_oCallbackDict ?? new Dictionary<ECallback, System.Action<CFirebaseManager, bool>>()
 		};
 	}
 	#endregion			// 클래스 함수
